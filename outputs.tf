9e236e1532943604df60b228c2e800cc7d6f71b5
Jon Ward <jon@samarkand.global>
2022-08-30 13:48:51 +0900

    add description to output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "realm_internal_ids" {
+  description = "Map of Realm names and internal IDs."
   value = {
     for realm in keys(var.realms) :
     realm => keycloak_realm.this[realm].internal_id
